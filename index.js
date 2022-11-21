a548f4baeecb030f7b09cf334135526814d0498f
Abdirahman Ahmad <115547577+Luunge@users.noreply.github.com>
2022-11-21 17:20:40 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,63 @@
+// let myZaad = {
+//     name: "Abdirahman",
+//     phone:"4864657",
+//     pin:4488,
+// };
+
+// let enteredPin = 4488;
+
+
+// if (enteredPin === myZaad.pin){
+//     console.log("Ku soo dhawoow Zaad Service");
+//     console.log("1. Eeg Hadhaagaaga");
+//     console.log("2. Lacag Dirid");
+//     } else{
+//         console.log("Maca Salaama");
+//     }
+
+
+
+
+
+
+
+
+
+
+
+    // from 6 to 12     ||  Good Morning
+    // from 13 to 18    ||  Good Afternoon
+    // from 19 to 5     ||  Good Night
+
+    // let time = 21
+
+    // if (time >= 6 && time <= 12) {
+    //     console.log("Good Morning Sir")
+    // } else if(time >= 13 && time <= 18) {
+    //     console.log("Good Afternoon Sir")
+    // } else {
+    //     console.log("Good Night Sir")
+    // }
+
+
+
+
+
+
+
+
+
+
+    // Before 8.30     ||  Welcome to the class
+    // 8.30 − 8.50     ||  You are late
+    // after 8.50      ||  Go to your home
+
+    let studentTime = 8.25;
+
+    if (studentTime <= 8.30) {
+        console.log("Welcome to the class")
+    } else if(studentTime < 8.50 && studentTime > 8.30) {
+        console.log("You are late")
+    } else {
+        console.log("Go to your home")
+    }
